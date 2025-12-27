f9263cac16a7a1b021b011b7380f2ea178b8c0ca
leticia-noh <leticianhonda@gmail.com>
2025-12-27 17:44:13 -0300

    fix: remove order column from card table

--- src/main/resources/db/changelog/migrations/db.changelog-202512271725.sql
+++ src/main/resources/db/changelog/migrations/db.changelog-202512271725.sql
@@ -6,7 +6,6 @@ CREATE TABLE card (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description VARCHAR(255) NOT NULL,
-    `order` int NOT NULL,
     board_column_id BIGINT NOT NULL,
 
     CONSTRAINT board_column__card_fk FOREIGN KEY (board_column_id) REFERENCES board_column(id) ON DELETE CASCADE
